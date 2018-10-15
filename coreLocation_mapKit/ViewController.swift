ec2278cd5846745fe6ee06df0fdd2adb49ca9734
Charles Moncada <charles.moncada@emoshiapps.com>
2018-10-15 14:13:30 -0300

    Initial Commit

---
+++ coreLocation_mapKit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  coreLocation_mapKit
+//
+//  Created by Charles Moncada on 15/10/18.
+//  Copyright © 2018 Charles Moncada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
